d990fb9e9b4ebc4333845b2a0cf1eef4de54a6d4
nene-hue <neomontshiwa2@gmail.com>
2025-04-05 17:36:50 +0200

    added javascript for the forecast

--- src/index.js
+++ src/index.js
@@ -56,6 +56,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "63a80bfto5abf3302bc64462b1e4f0ae";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -65,21 +72,29 @@ function getForecast(city) {
 function displayForecast(response) {
   let forecast = document.querySelector("#forecast");
 
-  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
 <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">⛅</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <div >
+        <img src="${
+          day.condition.icon_url
+        }" class="weather-forecast-icon"/></div>
         <div class="weather-forecast-temperatures">
-        <div class="weather-forecast-temperature"><strong>15°</strong></div> 
-        <div class="weather-forecast-temperature">9°</div></div>
+        <div class="weather-forecast-temperature"><strong>${Math.round(
+          day.temperature.maximum
+        )}°</strong></div> 
+        <div class="weather-forecast-temperature">${Math.round(
+          day.temperature.minimum
+        )}°</div></div>
       </div>
     `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
